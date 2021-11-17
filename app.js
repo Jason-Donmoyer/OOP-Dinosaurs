7e82ee8ba9832b52774ca20d46466537c5262138
Jason-Donmoyer <j.donmoyer523@gmail.com>
2021-11-17 17:49:11 -0500

    JS: -added Animal factory function -refactored Dino class to inherit from Animal -added Human class constructor

--- app.js
+++ app.js
@@ -2,16 +2,28 @@ const form = document.getElementById('dino-compare');
 const dinoGrid = document.getElementById('grid');
 const submitBtn = document.getElementById('btn');
 
-    // Create Dino Constructor
 
-    function Dino(species, weight, height, diet, where, when, fact) {
+    // Animal Factory Function
+    function Animal(species, weight, height, fact) {
         this.name = species;
         this.weight = weight;
         this.height = height;
+        this.fact = fact;
+        this.image = `images/${species.toLowerCase()}.png`;
+    }
+
+    // Create Dino Constructor
+
+    function Dino(species, weight, height, diet, where, when, fact) {
+        Animal.call(this, species, weight, height, fact);
+        // this.name = species;
+        // this.weight = weight;
+        // this.height = height;
         this.diet = diet;
         this.location = where;
         this.timePeriod = when;
-        this.fact = fact;
+        // this.fact = fact;
+        // this.image = `images/${species.toLowerCase()}.png`;
     }
 
 
@@ -19,8 +31,31 @@ const submitBtn = document.getElementById('btn');
 
     // Create Dino Objects
 
+    // Call to fetch dinosaur data from dino.json and create array of Dino objects
+    let dinosaurs = [];
+    fetch('dino.json')
+        .then(res => res.json())
+        .then(json => dinosaurs = json.Dinos.map(dinosaur => new Dino(
+            dinosaur.species, 
+            dinosaur.weight, 
+            dinosaur.height, 
+            dinosaur.diet, 
+            dinosaur.where, 
+            dinosaur.when, 
+            dinosaur.fact,
+            dinosaur.image)
+        ));
+
 
     // Create Human Object
+
+    function Human(name, weight, feet, inches, diet) {
+        Animal.call(this, 'human', weight);
+        this.height = (feet * 12) + inches;
+        this.diet = diet;
+    }
+
+
 
     // Use IIFE to get human data from form
 
@@ -39,19 +74,7 @@ const submitBtn = document.getElementById('btn');
 
     // Generate Tiles for each Dino in Array
 
-    // Call to fetch dinosaur data from dino.json and create array of Dino objects
-    let dinosaurs = [];
-    fetch('dino.json')
-        .then(res => res.json())
-        .then(json => dinosaurs = json.Dinos.map(dinosaur => new Dino(
-            dinosaur.species, 
-            dinosaur.weight, 
-            dinosaur.height, 
-            dinosaur.diet, 
-            dinosaur.where, 
-            dinosaur.when, 
-            dinosaur.fact)
-        ));
+    
   
         // Add tiles to DOM
 
