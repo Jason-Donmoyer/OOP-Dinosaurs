2658bac59cacadb3ab69f6dc0373e925948c168e
Jason-Donmoyer <j.donmoyer523@gmail.com>
2021-11-20 05:49:41 -0500

    JS: -added compareWeight function to Dino constructor -fixed ISSUE with getting value from dropdown menu -added variable to store human data after IIFE

--- app.js
+++ app.js
@@ -4,25 +4,26 @@ const submitBtn = document.getElementById('btn');
 
 
     // Animal Constructor
-    function Animal(species, weight, height, fact) {
+    function Animal(species, weight, height) {
         this.name = species;
         this.weight = weight;
         this.height = height;
-        this.fact = fact;
         this.image = `images/${species.toLowerCase()}.png`;
+        
     }
 
     // Create Dino Constructor
 
     function Dino(species, weight, height, diet, where, when, fact) {
-        Animal.call(this, species, weight, height, fact);
+        Animal.call(this, species, weight, height);
         // this.name = species;
         // this.weight = weight;
         // this.height = height;
         this.diet = diet;
         this.location = where;
         this.timePeriod = when;
-        // this.fact = fact;
+        this.fact = fact;
+        this.compareWeight = compareWeight;
         // this.image = `images/${species.toLowerCase()}.png`;
     }
 
@@ -50,34 +51,53 @@ const submitBtn = document.getElementById('btn');
     // Create Human Object
 
     function Human(name, weight, feet, inches, diet) {
-        Animal.call(this, name, weight);
+        Animal.apply(this, [name, weight]);
         this.height = (parseInt(feet) * 12) + parseInt(inches);
         this.diet = diet;
+        this.image = 'images/human.png';
     }
+
+    // Human.prototype = Object.create(Animal.prototype);
+    // Human.prototype.constructor = Human;
 
 
 
     // Use IIFE to get human data from form
     
+    let human = {};
+            
 
     function getHumanData() {
-        return (function() {
-        const humanName = document.getElementById('name').value;
-        const humanHeightInFeet = document.getElementById('feet').value;
-        const humanHeightInInches = document.getElementById('inches').value;
-        const humanWeight = document.getElementById('weight').value;
-        const humanDiet = document.getElementById('diet').value;
-        return new Human(humanName, 
+        return (function() { 
+            const humanName = document.getElementById('name').value;
+            const humanHeightInFeet = document.getElementById('feet').value;
+            const humanHeightInInches = document.getElementById('inches').value;
+            const humanWeight = parseInt(document.getElementById('weight').value);
+            const diet = document.getElementById('diet')
+            const humanDiet = diet.options[diet.selectedIndex].text;
+            human = new Human(humanName, 
                         humanWeight, 
                         humanHeightInFeet, 
                         humanHeightInInches, 
                         humanDiet);
+            return human;
         })();
     }   
 
 
     // Create Dino Compare Method 1
     // NOTE: Weight in JSON file is in lbs, height in inches. 
+    compareWeight = function(weight) {
+        let fact = '';
+        if (this.weight > weight) {
+            fact = `I am heavier than you by ${this.weight - weight} punds!`;
+        } else if (this.weight < weight) {
+            fact = `You are heavier than me by ${weight - this.weight} pounds!`;
+        } else {
+            fact = 'It looks like we are the same weight!';
+        }
+        return fact;
+    }
 
     
     // Create Dino Compare Method 2
@@ -104,6 +124,7 @@ const submitBtn = document.getElementById('btn');
 submitBtn.addEventListener('click', () => {
     form.style.display = 'none';
     const human = getHumanData();
-    console.log([human.name, human.weight, human.height, human.diet]);
+    console.log(human);
+    console.log([human.name, human.weight, human.height, human.diet, human.image]);
     grid.style.display = 'flex';
 });
